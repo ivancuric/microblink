d2f2075c9fe325c1d3f1b92de95b4c545d82329f
Ivan Čurić <ivan.curic@gmail.com>
2021-11-07 12:48:44 +0100

    Fix git hooks

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+/* eslint-env node */
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': ['eslint --cache --fix --max-warnings=0'],
+  '*.{ts,tsx}': [() => 'tsc --skipLibCheck --noEmit'],
+  '*.{js,jsx,ts,tsx,css,md}': ['prettier --write'],
+};
